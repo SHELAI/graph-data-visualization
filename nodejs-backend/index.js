49968984b14b815a974b9098ca7af3cd1d250cab
Helai <sinan.helai@capgemini.com>
2024-07-18 21:40:49 +0200

    initial setup for backend using nodejs

---
+++ nodejs-backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// Sample graph data
+let graphData = {
+    data: [
+      { name: 'A', description: 'This is a description of A', parent: '' },
+      { name: 'B', description: 'This is a description of B', parent: 'A' },
+      { name: 'C', description: 'This is a description of C', parent: 'A' },
+      { name: 'D', description: 'This is a description of D', parent: 'A' },
+      { name: 'B-1', description: 'This is a description of B-1', parent: 'B' },
+      { name: 'B-2', description: 'This is a description of B-2', parent: 'B' },
+      { name: 'B-3', description: 'This is a description of B-3', parent: 'B' },
+    ]
+  };
+
+app.get('/graph', (req, res) => {
+    res.json(graphData);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
